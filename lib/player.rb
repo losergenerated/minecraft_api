2412412b4c7ed3e37aec706db640006e29dac4f7
Kevin (KJ) Jacobson <kevindjacobson@gmail.com>
2016-07-24 19:56:11 -0700

    Added direction, pitch, rotation to Player.

--- lib/player.rb
+++ lib/player.rb
@@ -7,8 +7,20 @@ class Player
     @api.send("player.setTile(#{x},#{y},#{z})")
   end
 
+  def direction
+    @api.send_and_receive("player.getDirection()").to_f
+  end
+
+  def pitch
+    @api.send_and_receive("player.getPitch()").to_f
+  end
+
+  def rotation
+    @api.send_and_receive("player.getRotation()").to_f
+  end
+
   def location
     response = @api.send_and_receive("player.getTile()")
     response.split(',').map { |s| s.to_i }
-  end    
+  end
 end
